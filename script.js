38b8776279584e5b1f2bcf92a8226659c89666ae
Helen Maribel Interiano Salguero <elsasalguero@gmail.com>
2026-03-24 09:52:49 -0600

    fondo

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 
   alert(" BIENVENIDOS A LA CASA DEL SABOR");
+let nombre = "Helen";
+  let apellido = "Interiano";
+//   numerico
+let edad = 27;
+let nota = 98;
+// Boolean (verdadero falso)
+let estaActivo = true;
+let estaEliminado = false;
+// Null (vacio intencionalemente)
+let direccion = null;
+// undefined (sin valor aun)
+
+console.log("hola soy la creadora de esta pagina  " + nombre + " "   );
